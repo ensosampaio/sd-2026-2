70687aaf41475233dd3de5b0a6dc79b7469bd743
Enzo Sampaio <enzosenzos9@gmail.com>
2026-08-27 18:55:21 -0300

    Cliente grpc

--- aula 4/cliente_grpc.py
+++ aula 4/cliente_grpc.py
@@ -2,6 +2,6 @@ import grpc
 import servico_pb2, servico_pb2_grpc
 
 with grpc.insecure_channel("127.0.0.1:50051") as canal:
-    stub = servico_pb2_grpc.CalculadoraStub(canal)
+    stub = servico_pb2_grpc.calculadoraStub(canal)
     resposta = stub.Somar(servico_pb2.Operandos(a=2, b=3))   # parece local!
     print("2 + 3 =", resposta.valor)
